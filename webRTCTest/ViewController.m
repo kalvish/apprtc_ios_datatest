5ba1fee238f6fc7681fc78a8e2a9b0171d0cc41c
ganuka <ganuke@Krishans-Mac-mini.local>
2016-07-07 15:23:25 +0530

    initial commit

---
+++ webRTCTest/ViewController.m
@@ -0,0 +1,121 @@
+//
+//  ViewController.m
+//  webRTCTest
+//
+//  Created by ganuka on 5/10/16.
+//  Copyright © 2016 ganuka. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+
+    /* RTCEAGLVideoViewDelegate provides notifications on video frame dimensions */
+    [self.remoteView setDelegate:self];
+    [self.localView setDelegate:self];
+    
+   }
+
+- (void) viewWillDisappear:(BOOL)animated {
+    [self disconnect];
+}
+
+- (void)applicationWillResignActive:(UIApplication*)application {
+    [self disconnect];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)appClient:(ARDAppClient *)client didChangeState:(ARDAppClientState)state {
+    switch (state) {
+        case kARDAppClientStateConnected:
+            NSLog(@"Client connected.");
+            break;
+        case kARDAppClientStateConnecting:
+            NSLog(@"Client connecting.");
+            break;
+        case kARDAppClientStateDisconnected:
+            NSLog(@"Client disconnected.");
+            [self remoteDisconnected];
+            break;
+    }
+}
+
+- (void)appClient:(ARDAppClient *)client didReceiveLocalVideoTrack:(RTCVideoTrack *)localVideoTrack {
+    if (self.localVideoTrack) {
+        [self.localVideoTrack removeRenderer:self.localView];
+        self.localVideoTrack = nil;
+        [self.localView renderFrame:nil];
+    }
+    self.localVideoTrack = localVideoTrack;
+    [self.localVideoTrack addRenderer:self.localView];
+
+}
+
+- (void)appClient:(ARDAppClient *)client didReceiveRemoteVideoTrack:(RTCVideoTrack *)remoteVideoTrack {
+    self.remoteVideoTrack = remoteVideoTrack;
+    [self.remoteVideoTrack addRenderer:self.remoteView];
+}
+
+- (void)appClient:(ARDAppClient *)client didError:(NSError *)error {
+    /* Handle the error */
+    UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:nil
+                                                        message:[NSString stringWithFormat:@"%@", error]
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+    [alertView show];
+    [self disconnect];
+}
+
+
+- (void)videoView:(RTCEAGLVideoView *)videoView didChangeVideoSize:(CGSize)size {
+    /* resize self.localView or self.remoteView based on the size returned */
+}
+
+- (IBAction)onclickstart:(id)sender {
+    NSString * temp = self.roomText.text;
+    
+    //Connect to the room
+    [self disconnect];
+    /* Initializes the ARDAppClient with the delegate assignment */
+    self.client = [[ARDAppClient alloc] initWithDelegate:self];
+    [self.client setServerHostUrl:@"https://apprtc.appspot.com"];
+    [self.client connectToRoomWithId:temp options:nil];
+
+    
+}
+
+- (void)disconnect {
+    if (self.client) {
+        if (self.localVideoTrack) [self.localVideoTrack removeRenderer:self.localView];
+        if (self.remoteVideoTrack) [self.remoteVideoTrack removeRenderer:self.remoteView];
+        self.localVideoTrack = nil;
+        [self.localView renderFrame:nil];
+        self.remoteVideoTrack = nil;
+        [self.remoteView renderFrame:nil];
+        [self.client disconnect];
+    }
+}
+
+- (void)remoteDisconnected {
+    if (self.remoteVideoTrack) [self.remoteVideoTrack removeRenderer:self.remoteView];
+    self.remoteVideoTrack = nil;
+    [self.remoteView renderFrame:nil];
+    //[self videoView:self.localView didChangeVideoSize:self.localVideoSize];
+    
+}
+
+@end
